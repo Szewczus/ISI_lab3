1495889893feaf5d8ace48773e28c959a6951bf0
Ewus <ewus9999@gmail.com>
2022-04-02 17:47:48 +0200

    dodawanie do tabelki danych

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.GridPane;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -36,17 +37,26 @@ public class Controller implements Initializable {
     @FXML
     private GridPane miejsce_na_dane;
 
-    private TableColumn col1;
+    @FXML
+    private GridPane miejsce_na_przyciski;
+
+    @FXML
+    private Button dodaj;
+
+    static String nazwy_kolumn[] ={"nazwa producenta", "przekątna ekranu", "rozdzielczość ekranu", "rodzaj powierzchni ekranu", "czy ekran jest dotykowy", "nazwa procesora",  "liczba rdzeni fizycznych", "prędkość taktowania MHz", "wielkość pamięci RAM", "pojemność dysku", "rodzaj dysku", "nazwa układu graficznego", "pamięć układu graficznego", "nazwa systemu operacyjnego", "rodzaj napędu fizycznego w komputerze"};
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String nazwy_kolumn[] ={"nazwa producenta", "przekątna ekranu", "rozdzielczość ekranu", "rodzaj powierzchni ekranu", "czy ekran jest dotykowy", "nazwa procesora",  "liczba rdzeni fizycznych", "prędkość taktowania MHz", "wielkość pamięci RAM", "pojemność dysku", "rodzaj dysku", "nazwa układu graficznego", "pamięć układu graficznego", "nazwa systemu operacyjnego", "rodzaj napędu fizycznego w komputerze"};
 
-        for(int i=0; i<nazwy_kolumn.length; i++){
-            col1 = new TableColumn(nazwy_kolumn[i]);
+        int j = 1;
+        for(String columHeader : nazwy_kolumn){
+            TableColumn<Data, String> col1 = new TableColumn<>(columHeader);
             col1.setMinWidth(100);
+            col1.setCellValueFactory(new PropertyValueFactory<Data, String>("col"+j));
+            col1.setCellFactory(TextFieldTableCell.<Data>forTableColumn());
             tableView.getColumns().add(col1);
+            j++;
         }
 
 
@@ -59,7 +69,9 @@ public class Controller implements Initializable {
             miejsce_na_dane.add(textField, i, 3);
         }
 
+
         miejsce_na_dane.setVisible(false);
+        miejsce_na_przyciski.setVisible(false);
 
 
 
@@ -68,10 +80,8 @@ public class Controller implements Initializable {
             @Override
             public void handle(ActionEvent event) {
                 miejsce_na_dane.setVisible(false);
-                List <Data> danetxtList = wczytajDaneZPlikuTXT();
-                ObservableList<Data> data1 = FXCollections.observableArrayList(danetxtList);
-                setcellValues(nazwy_kolumn);
-                tableView.setItems(data1);
+                miejsce_na_przyciski.setVisible(false);
+                wczytajZPlikuIUmiescWTabeli(nazwy_kolumn);
             }
         });
 
@@ -79,21 +89,41 @@ public class Controller implements Initializable {
             @Override
             public void handle(ActionEvent event) {
                 miejsce_na_dane.setVisible(true);
+                miejsce_na_przyciski.setVisible(true);
+                wczytajZPlikuIUmiescWTabeli(nazwy_kolumn);
+            }
+        });
+
+        dodaj.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //List <Data> danetxtList = wczytajDaneZPlikuTXT();
+                List <Data> danetxtList = tableView.getItems();
+                String cecha []= new String[24];
+                Data data1 = new Data();
+                for(int i=0; i<15; i++){
+                    TextField textField = (TextField)miejsce_na_dane.getChildren().get(i);
+                    cecha[i]=textField.getText();
+                }
+                setData1(data1, cecha);
+
+                danetxtList.add(data1);
+                ObservableList<Data> dane = FXCollections.observableArrayList(danetxtList);
+                //setcellValues(nazwy_kolumn);
+                tableView.setItems(dane);
             }
         });
 
 
     }
 
-    private void setcellValues(String nazwy_kolumn[]) {
-        int i=1;
-        for(Object column : tableView.getColumns()) {
-            TableColumn tableColumn = (TableColumn) column;
-            tableColumn.setCellValueFactory(new PropertyValueFactory<Data, String>("col"+i));
-            i++;
-        }
-
+    private void wczytajZPlikuIUmiescWTabeli(String[] nazwy_kolumn) {
+        List <Data> danetxtList = wczytajDaneZPlikuTXT();
+        ObservableList<Data> data1 = FXCollections.observableArrayList(danetxtList);
+        //setcellValues(nazwy_kolumn);
+        tableView.setItems(data1);
     }
+
 
     private List<Data> wczytajDaneZPlikuTXT() {
         List<Data> data = new ArrayList<>();
@@ -102,21 +132,7 @@ public class Controller implements Initializable {
             while (line != null) {
                 Data data1 = new Data();
                 String [] cecha = line.split(";", -1);
-                data1.setCol1(cecha[0]);
-                data1.setCol2(cecha[1]);
-                data1.setCol3(cecha[2]);
-                data1.setCol4(cecha[3]);
-                data1.setCol5(cecha[4]);
-                data1.setCol6(cecha[5]);
-                data1.setCol7(cecha[6]);
-                data1.setCol8(cecha[7]);
-                data1.setCol9(cecha[8]);
-                data1.setCol10(cecha[9]);
-                data1.setCol11(cecha[10]);
-                data1.setCol12(cecha[11]);
-                data1.setCol13(cecha[12]);
-                data1.setCol14(cecha[13]);
-                data1.setCol15(cecha[14]);
+                setData1(data1, cecha);
                 data.add(data1);
                 line = br.readLine();
             }
@@ -128,4 +144,33 @@ public class Controller implements Initializable {
         }
         return data;
     }
+
+    private void setData1(Data data1, String[] cecha) {
+        data1.setCol1(cecha[0]);
+        data1.setCol2(cecha[1]);
+        data1.setCol3(cecha[2]);
+        data1.setCol4(cecha[3]);
+        data1.setCol5(cecha[4]);
+        data1.setCol6(cecha[5]);
+        data1.setCol7(cecha[6]);
+        data1.setCol8(cecha[7]);
+        data1.setCol9(cecha[8]);
+        data1.setCol10(cecha[9]);
+        data1.setCol11(cecha[10]);
+        data1.setCol12(cecha[11]);
+        data1.setCol13(cecha[12]);
+        data1.setCol14(cecha[13]);
+        data1.setCol15(cecha[14]);
+    }
+
+
+    private List<Data> wczytajDaneZPlikuXML(){
+        List<Data> data = new ArrayList<>();
+        return data;
+    }
+
+
+
+
+
 }
