d0f93c57b9316cfa05d824d20f153c468bc36a04
Ewus <ewus9999@gmail.com>
2022-04-03 15:42:10 +0200

    dodawanie do tabelki danych

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,9 +10,10 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.GridPane;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,11 +44,16 @@ public class Controller implements Initializable {
     @FXML
     private Button dodaj;
 
+
     static String nazwy_kolumn[] ={"nazwa producenta", "przekątna ekranu", "rozdzielczość ekranu", "rodzaj powierzchni ekranu", "czy ekran jest dotykowy", "nazwa procesora",  "liczba rdzeni fizycznych", "prędkość taktowania MHz", "wielkość pamięci RAM", "pojemność dysku", "rodzaj dysku", "nazwa układu graficznego", "pamięć układu graficznego", "nazwa systemu operacyjnego", "rodzaj napędu fizycznego w komputerze"};
 
+    ClassReader classReader;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        classReader = new ClassReader(Data.class);
+        classReader.readClassMethods(); //metoda przechowuje metody klasy Data
 
         int j = 1;
         for(String columHeader : nazwy_kolumn){
@@ -55,23 +61,29 @@ public class Controller implements Initializable {
             col1.setMinWidth(100);
             col1.setCellValueFactory(new PropertyValueFactory<Data, String>("col"+j));
             col1.setCellFactory(TextFieldTableCell.<Data>forTableColumn());
+            col1.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Data, String>>() {
+                @Override
+                public void handle(TableColumn.CellEditEvent<Data, String> event) {
+                    Data data = event.getTableView().getItems().get(event.getTablePosition().getRow());
+                    Method method = classReader.getSetMethods().get(event.getTablePosition().getColumn());
+                    try {
+                        method.invoke(data, event.getNewValue());   //computer.setManufacturer(t.getNewValue());
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
             tableView.getColumns().add(col1);
             j++;
         }
-
-
         tableView.setMinWidth(500);
 
 
-        for(int i=0; i<nazwy_kolumn.length; i++){
-            TextField textField = new TextField();
-            textField.setPromptText(nazwy_kolumn[i]);
-            miejsce_na_dane.add(textField, i, 3);
-        }
-
-
-        miejsce_na_dane.setVisible(false);
-        miejsce_na_przyciski.setVisible(false);
+//        for(int i=0; i<nazwy_kolumn.length; i++){
+//            TextField textField = new TextField();
+//            textField.setPromptText(nazwy_kolumn[i]);
+//            miejsce_na_dane.add(textField, i, 3);
+//        }
 
 
 
@@ -79,8 +91,6 @@ public class Controller implements Initializable {
         wczytaj_txt.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                miejsce_na_dane.setVisible(false);
-                miejsce_na_przyciski.setVisible(false);
                 wczytajZPlikuIUmiescWTabeli(nazwy_kolumn);
             }
         });
@@ -89,16 +99,33 @@ public class Controller implements Initializable {
             @Override
             public void handle(ActionEvent event) {
                 miejsce_na_dane.setVisible(true);
-                miejsce_na_przyciski.setVisible(true);
-                wczytajZPlikuIUmiescWTabeli(nazwy_kolumn);
+                List <Data> danetxtList = tableView.getItems();
+                FileWriter w = null;
+                try {
+                    w = new FileWriter("katalog2.txt");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                for(Data data: danetxtList){
+                    try {
+                        w.write(data.getCol1() + ";" + data.getCol2()+";"+ data.getCol3()+";"+ data.getCol4()+";"+data.getCol5() + ";" + data.getCol6() + ";" + data.getCol7() + ";" + data.getCol8() + ";" + data.getCol9() + ";" + data.getCol10() + ";" + data.getCol11() +";"+ data.getCol12() +";" + data.getCol13() + ";"+"\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    w.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
         dodaj.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                //List <Data> danetxtList = wczytajDaneZPlikuTXT();
-                List <Data> danetxtList = tableView.getItems();
+                /** to służyło do odczytywania z textfeildów daych i wstawiania do tabelki teraz jest edytowane bezpośrednio z poziomu tabeli **/
+/*              List <Data> danetxtList = tableView.getItems();
                 String cecha []= new String[24];
                 Data data1 = new Data();
                 for(int i=0; i<15; i++){
@@ -111,8 +138,13 @@ public class Controller implements Initializable {
                 ObservableList<Data> dane = FXCollections.observableArrayList(danetxtList);
                 //setcellValues(nazwy_kolumn);
                 tableView.setItems(dane);
+                tableView.setEditable(true);*/
+
+                /** dodanie nowego wiersza  **/
+                tableView.getItems().add(new Data());
             }
         });
+
 
 
     }
@@ -120,19 +152,20 @@ public class Controller implements Initializable {
     private void wczytajZPlikuIUmiescWTabeli(String[] nazwy_kolumn) {
         List <Data> danetxtList = wczytajDaneZPlikuTXT();
         ObservableList<Data> data1 = FXCollections.observableArrayList(danetxtList);
-        //setcellValues(nazwy_kolumn);
         tableView.setItems(data1);
+        tableView.setEditable(true);
     }
 
 
     private List<Data> wczytajDaneZPlikuTXT() {
         List<Data> data = new ArrayList<>();
+
         try(BufferedReader br = new BufferedReader(new FileReader("katalog.txt"))) {
             String line = br.readLine();
             while (line != null) {
                 Data data1 = new Data();
                 String [] cecha = line.split(";", -1);
-                setData1(data1, cecha);
+                setData1(data1, cecha, classReader);
                 data.add(data1);
                 line = br.readLine();
             }
@@ -141,26 +174,20 @@ public class Controller implements Initializable {
 
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
         return data;
     }
 
-    private void setData1(Data data1, String[] cecha) {
-        data1.setCol1(cecha[0]);
-        data1.setCol2(cecha[1]);
-        data1.setCol3(cecha[2]);
-        data1.setCol4(cecha[3]);
-        data1.setCol5(cecha[4]);
-        data1.setCol6(cecha[5]);
-        data1.setCol7(cecha[6]);
-        data1.setCol8(cecha[7]);
-        data1.setCol9(cecha[8]);
-        data1.setCol10(cecha[9]);
-        data1.setCol11(cecha[10]);
-        data1.setCol12(cecha[11]);
-        data1.setCol13(cecha[12]);
-        data1.setCol14(cecha[13]);
-        data1.setCol15(cecha[14]);
+    private void setData1(Data data1, String[] cecha, ClassReader classReader) throws InvocationTargetException, IllegalAccessException {
+        int i=0;
+        for(Method method : classReader.getSetMethods()){
+            method.invoke(data1, cecha[i]); //zamiast    data1.setCol1(cecha[0]); data1.setCol2(cecha[1]); itd
+            i++;
+        }
     }
 
 
